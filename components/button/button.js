59acab596b2ba4a2a8cbc100d713e67302a40787
tyj1113 <1991383016@qq.com>
2021-02-06 15:10:18 +0800

    封装button组件

---
+++ components/button/button.js
@@ -0,0 +1,12 @@
+Component({
+  properties:{
+    type:{
+      type: String,            //按钮类型控制大小 不传就是默认 传mini 宽度为小按钮
+      value: ''
+    },
+    text:{
+      type: String,
+      value: ''
+    },
+  }
+})
