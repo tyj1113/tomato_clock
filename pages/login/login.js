4154639ca61d7b4c2bf430307cd98ed4f085635b
tyj1113 <1991383016@qq.com>
2021-02-08 00:51:46 +0800

    完成登录功能

--- pages/login/login.js
+++ pages/login/login.js
@@ -1,66 +1,27 @@
-// pages/login/login.js
+import http from '../../lib/http'
+const { app_id, app_secret } = getApp().globalData
+
 Page({
+login(e){
+  let encrypted_data = e.detail.encryptedData
+  let iv = e.detail.iv,code;
+  wx.login({
+    success(res){
+      code=res.code
+      http.post('/sign_in/mini_program_user', {
+        code,
+        iv,
+        encrypted_data,
+        app_id,
+        app_secret,
+      }).then(response => {
+        wx.setStorageSync('me', response.data.resource)
+        wx.setStorageSync('X-token', response.header["X-token"])
+        wx.reLaunch({ url: "/pages/home/home" })
 
-  /**
-   * 页面的初始数据
-   */
-  data: {
+          })
+    }
+  })
 
-  },
-
-  /**
-   * 生命周期函数--监听页面加载
-   */
-  onLoad: function (options) {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面显示
-   */
-  onShow: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面隐藏
-   */
-  onHide: function () {
-
-  },
-
-  /**
-   * 生命周期函数--监听页面卸载
-   */
-  onUnload: function () {
-
-  },
-
-  /**
-   * 页面相关事件处理函数--监听用户下拉动作
-   */
-  onPullDownRefresh: function () {
-
-  },
-
-  /**
-   * 页面上拉触底事件的处理函数
-   */
-  onReachBottom: function () {
-
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
-
-  }
+}
 })
