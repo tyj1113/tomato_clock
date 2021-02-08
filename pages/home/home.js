c54445e8141dbb029bcd09ae7229ea769a8beb76
tyj1113 <1991383016@qq.com>
2021-02-08 20:57:53 +0800

    首页接入数据完成,创建修改删除功能完成

--- pages/home/home.js
+++ pages/home/home.js
@@ -1,15 +1,59 @@
+import http from '../../lib/http'
 Page({
     data: {
-        visible: false
+        createVisible: false,
+        updateTextVisible: false,
+        list:[],
+        content:'',
+        updateTextId:'',
+        updateTextIndex:''
     },
-    showConfirm() {
-        this.setData({visible: true})
+    onShow(){
+      http.get('/todos?completed=false').then((res)=>{
+        this.setData({list:res.data.resources})
+        console.log(res.data.resources)
+      })
     },
-    update(e) {
-        console.log(e.detail)
-        this.hideConfirm()
+  completed(e){
+    const index = e.target.dataset.index
+    http.put(`/todos/${e.target.dataset.id}`, {completed:true}).then((res)=>{
+      this.data.list[index]=res.data.resource
+      this.setData({list:this.data.list})
+    })
+  },
+    showTextConfirm(e){
+        this.setData({updateTextVisible: true,content:e.target.dataset.test})
+        this.data.updateTextId=e.target.dataset.id
+        this.data.updateTextIndex=e.target.dataset.index
     },
-    hideConfirm() {
-        this.setData({visible: false})
+    updateText(e){
+        const index = this.data.updateTextIndex
+      http.put(`/todos/${this.data.updateTextId}`,{ completed: false, description:`${e.detail}`}).then(
+          (res)=>{
+              this.data.list[index]=res.data.resource
+              this.setData({list:this.data.list})
+              this.hideUpdateConfirm()
+          }
+      )
+    },
+    showCreateConfirm() {
+        this.setData({createVisible: true})
+    },
+    create(e) {
+      http.post('/todos', { description: e.detail }).then((res) => {  
+        console.log(res)
+        //接口写的不好resource一会有s一会没有
+        //resource 是对象不能用concat 转为数组
+        const item = [res.data.resource]
+        this.data.list = this.data.list.concat(item)
+        this.setData({list:this.data.list})
+      })
+        this.hideCreateConfirm()
+    },
+    hideCreateConfirm() {
+        this.setData({createVisible: false})
+    },
+    hideUpdateConfirm() {
+        this.setData({updateTextVisible: false})
     }
 })
