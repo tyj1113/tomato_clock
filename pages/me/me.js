18eadc9ee1878e01967e09401d7c3b7f6e5ea5b7
tyj1113 <1991383016@qq.com>
2021-02-09 20:50:20 +0800

    me页面完成历史接入接口

--- pages/me/me.js
+++ pages/me/me.js
@@ -1,7 +1,16 @@
-// pages/me/me.js
+import http from '../../lib/http'
 Page({
   data: {
-    tab: "tomato",
+    tab: "task",
+      todos:{}
+  },
+  onShow(){
+//     http.get('/tomatoes',{ is_group: "yes" }).then((res)=>{
+//     })
+    http.get('/todos',{ is_group: "yes" }).then((res)=>{
+      this.setData({todos:res.data.resources})
+        console.log(res.data.resources)
+    })
   },
   changeTab(e){
 this.setData({tab:e.target.dataset.name})
