aa92587ad87cd9fe2e276cb902a8e0145974ac1e
tyj1113 <1991383016@qq.com>
2021-02-10 14:34:41 +0800

    登录页添加体验页面跳转按钮

--- pages/login/login.js
+++ pages/login/login.js
@@ -1,27 +1,29 @@
 import http from '../../lib/http'
-const { app_id, app_secret } = getApp().globalData
+const {app_id, app_secret} = getApp().globalData
 
 Page({
-login(e){
-  let encrypted_data = e.detail.encryptedData
-  let iv = e.detail.iv,code;
-  wx.login({
-    success(res){
-      code=res.code
-      http.post('/sign_in/mini_program_user', {
-        code,
-        iv,
-        encrypted_data,
-        app_id,
-        app_secret,
-      }).then(response => {
-        wx.setStorageSync('me', response.data.resource)
-        wx.setStorageSync('X-token', response.header["X-token"])
-        wx.reLaunch({ url: "/pages/home/home" })
+    login(e) {
+        let encrypted_data = e.detail.encryptedData
+        let iv = e.detail.iv, code;
+        wx.login({
+            success(res) {
+                code = res.code
+                http.post('/sign_in/mini_program_user', {
+                    code,
+                    iv,
+                    encrypted_data,
+                    app_id,
+                    app_secret,
+                }).then(response => {
+                    wx.setStorageSync('me', response.data.resource)
+                    wx.setStorageSync('X-token', response.header["X-token"])
+                    wx.reLaunch({url: "/pages/home/home"})
 
-          })
+                })
+            }
+        })
+    },
+    experience() {
+        wx.navigateTo({url: "/pages/experience/experience",})
     }
-  })
-
-}
 })
